4085397ad263b7348042044033935f2554b6a27f
fernando <altraciano@outlook.com>
2018-01-11 11:40:55 +0100

    Starting with reimplementation of vector's test

---
+++ src/tests/test_vector.h
@@ -0,0 +1,12 @@
+#ifndef TEST_VECTOR_H_
+#define TEST_VECTOR_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "../../headers/utils.h"
+#include "../../headers/vector.h"
+#include "../../headers/students_group.h"
+
+void test_vector_suite();
+
+#endif
